32e44ec33a4ee0b4aabc67f568677536c0b2bd1b
Lamboprogramming <gillespielamar@gmail.com>
2024-11-07 18:04:33 -0500

    contact info size and style changes

--- src/app/components/ContactInfo.jsx
+++ src/app/components/ContactInfo.jsx
@@ -5,20 +5,21 @@ import Link from 'next/link'
 
 export default function ContactInfo() {
   return (
-    <div className='mt-5'>
-       {/* Contact Info */}
-       <div className="items-center justify-center mt-6 mb-12 sm:space-x-8">
-       <div className="flex items-center justify-center space-x-2 font-serif text-2xl font-bold tracking-wide text-brown-900">
-         <FaPhoneAlt className="" />
-         <span>(888) 542 - 5855</span>
-       </div>
-       <div className="flex items-center justify-center mt-6 space-x-2 font-serif text-2xl font-bold tracking-wider text-brown-900 ">
-         <MdEmail className="" />
-         <Link href="mailto:Support@kiwinutzcoffee.com">
-         <span>Support@kiwinutzcoffee.com</span>
-         </Link>
-       </div>
-     </div>
-     </div>
+    <div className='mt-4'>
+      {/* Contact Info */}
+      <div className="flex flex-col items-center justify-center mt-6 mb-12 space-y-4 sm:mt-12 sm:flex-row sm:space-x-8 sm:space-y-0">
+        <div className="flex items-center mb-4 space-x-2 font-serif text-2xl tracking-wide sm:mb-0 sm:text-xl text-brown-900">
+          <FaPhoneAlt />
+          <span>(888) 542 - 5855</span>
+        </div>
+        
+        <div className="flex items-center space-x-2 font-serif text-2xl tracking-wider sm:text-xl text-brown-900">
+          <MdEmail />
+          <Link href="mailto:Support@kiwinutzcoffee.com">
+            <span>Support@kiwinutzcoffee.com</span>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
