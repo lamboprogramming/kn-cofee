75a294df41d99f7fb0890ae25b3166878f3c1a31
Lamboprogramming <gillespielamar@gmail.com>
2024-11-07 18:46:14 -0500

    contact size changes for mobile

--- src/app/components/ContactInfo.jsx
+++ src/app/components/ContactInfo.jsx
@@ -7,13 +7,13 @@ export default function ContactInfo() {
   return (
     <div className='mt-4'>
       {/* Contact Info */}
-      <div className="flex flex-col items-center justify-center mt-6 mb-12 space-y-4 sm:mt-12 sm:flex-row sm:space-x-8 sm:space-y-0">
-        <div className="flex items-center mb-4 space-x-2 font-serif text-2xl tracking-wide sm:mb-0 sm:text-xl text-brown-900">
+      <div className="flex flex-col items-center justify-center mt-4 mb-6 space-y-4 sm:mt-8 sm:flex-row sm:space-x-8 sm:space-y-0">
+        <div className="flex items-center mb-4 space-x-2 font-serif text-lg font-bold tracking-wide sm:mb-0 text-brown-900">
           <FaPhoneAlt />
           <span>(888) 542 - 5855</span>
         </div>
         
-        <div className="flex items-center space-x-2 font-serif text-2xl tracking-wider sm:text-xl text-brown-900">
+        <div className="flex items-center space-x-2 font-serif text-lg tracking-wider text-brown-900">
           <MdEmail />
           <Link href="mailto:Support@kiwinutzcoffee.com">
             <span>Support@kiwinutzcoffee.com</span>
