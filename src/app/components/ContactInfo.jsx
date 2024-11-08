493ecac37c2b46202216b2384c4a7340d6f70e34
Lamboprogramming <gillespielamar@gmail.com>
2024-11-07 19:01:05 -0500

    background color added to contactinfo

--- src/app/components/ContactInfo.jsx
+++ src/app/components/ContactInfo.jsx
@@ -5,16 +5,16 @@ import Link from 'next/link'
 
 export default function ContactInfo() {
   return (
-    <div className='mt-4'>
+    <div className='p-4 mt-4 bg-brown-500'>
       {/* Contact Info */}
       <div className="flex flex-col items-center justify-center mt-4 mb-6 space-y-4 sm:mt-8 sm:flex-row sm:space-x-8 sm:space-y-0">
-        <div className="flex items-center mb-4 space-x-2 font-serif text-lg font-bold tracking-wide sm:mb-0 text-brown-900">
+        <div className="flex items-center mb-4 space-x-2 font-serif text-lg font-bold tracking-wide text-white sm:mb-0">
           <FaPhoneAlt />
           <span>(888) 542 - 5855</span>
         </div>
         
-        <div className="flex items-center space-x-2 font-serif text-lg tracking-wider text-brown-900">
-          <MdEmail />
+        <div className="flex items-center space-x-2 font-serif text-lg font-bold tracking-wider text-white">
+          <MdEmail className=''/>
           <Link href="mailto:Support@kiwinutzcoffee.com">
             <span>Support@kiwinutzcoffee.com</span>
           </Link>
