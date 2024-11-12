71bdd3f2b79bce64eee931ba7a5063add0eafc3b
Lamboprogramming <gillespielamar@gmail.com>
2024-11-11 20:21:17 -0500

    favicon changes

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,9 +1,9 @@
 'use client';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import emailjs from 'emailjs-com';
 import ContactInfo from './ContactInfo';
 
 export default function Contact() {
-  // State for form inputs
   const [formData, setFormData] = useState({
     email: '',
     firstName: '',
@@ -12,39 +12,35 @@ export default function Contact() {
     companySize: '',
     phoneNumber: '',
     state: '',
+    comments: '',
   });
-
-  // State for submission feedback
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [error, setError] = useState('');
+  const form = useRef();
 
-  // Handle input changes
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-  
-    try {
-      const response = await fetch('src/app/api/send-email/route.js', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(formData),
-      });
-  
-      if (response.ok) {
-        console.log('Email sent successfully');
+
+    emailjs.sendForm(
+      'service_bt2iolr',
+      'template_67d4p1q',
+      form.current,
+      '3R0bhVy61UmW0fUQL'
+    ).then(
+      () => {
         setIsSubmitted(true);
         setError('');
-      } else {
-        throw new Error('Failed to send message');
+      },
+      (error) => {
+        console.error('Failed to send message:', error);
+        setError('Failed to send message. Please try again later.');
       }
-    } catch (error) {
-      console.error('Failed to send email:', error);
-      setError('Failed to send message. Please try again later.');
-    }
+    );
   };
 
   return (
@@ -63,116 +59,126 @@ export default function Contact() {
             {isSubmitted ? (
               <p className="text-center text-green-600">Thank you for contacting us! We’ll get back to you shortly.</p>
             ) : (
-              <form onSubmit={handleSubmit} className="p-4 space-y-4 tracking-widest">
-                {/* Email field */}
+              <form ref={form} onSubmit={handleSubmit} className="p-4 space-y-4 tracking-widest">
+                {/* Email Field */}
                 <div className="relative z-0 w-full mb-5 group">
                   <input
                     type="email"
                     name="email"
-                    id="email"
                     value={formData.email}
                     onChange={handleInputChange}
-                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-brown-500 peer"
+                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-brown-500 peer"
                     placeholder=" "
                     required
                   />
-                  <label htmlFor="email" className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">Email</label>
+                  <label className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">Email</label>
                 </div>
 
-                {/* First Name field */}
+                {/* First Name Field */}
                 <div className="relative z-0 w-full mb-5 group">
                   <input
                     type="text"
                     name="firstName"
-                    id="firstName"
                     value={formData.firstName}
                     onChange={handleInputChange}
-                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-brown-500 peer"
+                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-brown-500 peer"
                     placeholder=" "
                     required
                   />
-                  <label htmlFor="firstName" className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">First Name</label>
+                  <label className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">First Name</label>
                 </div>
 
-                {/* Last Name field */}
+                {/* Last Name Field */}
                 <div className="relative z-0 w-full mb-5 group">
                   <input
                     type="text"
                     name="lastName"
-                    id="lastName"
                     value={formData.lastName}
                     onChange={handleInputChange}
-                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-brown-500 peer"
+                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-brown-500 peer"
                     placeholder=" "
                     required
                   />
-                  <label htmlFor="lastName" className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">Last Name</label>
+                  <label className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">Last Name</label>
                 </div>
 
-                {/* Company field */}
+                {/* Company Field */}
                 <div className="relative z-0 w-full mb-5 group">
                   <input
                     type="text"
                     name="company"
-                    id="company"
                     value={formData.company}
                     onChange={handleInputChange}
-                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-brown-500 peer"
+                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-brown-500 peer"
                     placeholder=" "
-                    required
                   />
-                  <label htmlFor="company" className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">Company Name</label>
+                  <label className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">Company</label>
                 </div>
 
-                {/* Company Size dropdown */}
-                <div className="relative z-0 w-full mb-5 group">
-                  <select
-                    name="companySize"
-                    id="companySize"
-                    value={formData.companySize}
-                    onChange={handleInputChange}
-                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-brown-500 peer"
-                    required
-                  >
-                    <option value="" disabled hidden></option>
-                    <option value="1-10">1-10 employees</option>
-                    <option value="11-50">11-50 employees</option>
-                    <option value="51-200">51-200 employees</option>
-                    <option value="201-500">201-500 employees</option>
-                    <option value="500+">500+ employees</option>
-                  </select>
-                  <label htmlFor="companySize" className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">Company Size</label>
-                </div>
+            {/* Company Size dropdown */}
+            <div className="relative z-0 w-full mb-5 group">
+            <select
+            name="companySize"
+            value={formData.companySize}
+            onChange={handleInputChange}
+            className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-brown-500 peer"
+            required
+            >
+            <option value="" disabled hidden></option>
+            <option value="1-10">1-10 employees</option>
+            <option value="11-50">11-50 employees</option>
+            <option value="51-200">51-200 employees</option>
+            <option value="201-500">201-500 employees</option>
+            <option value="500+">500+ employees</option>
+            </select>
+          <label className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform          -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">
+           Company Size
+          </label>
+            </div>
 
-                {/* Phone Number field */}
+
+                {/* Phone Number Field */}
                 <div className="relative z-0 w-full mb-5 group">
                   <input
-                    type="tel"
+                    type="text"
                     name="phoneNumber"
-                    id="phoneNumber"
                     value={formData.phoneNumber}
                     onChange={handleInputChange}
-                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-brown-500 peer"
+                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-brown-500 peer"
                     placeholder=" "
                     required
                   />
-                  <label htmlFor="phoneNumber" className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">Phone Number</label>
+                  <label className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">Phone Number</label>
                 </div>
 
-                {/* State field */}
+                {/* State Field */}
                 <div className="relative z-0 w-full mb-5 group">
                   <input
                     type="text"
                     name="state"
-                    id="state"
                     value={formData.state}
                     onChange={handleInputChange}
-                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white focus:outline-none focus:ring-0 focus:border-brown-500 peer"
+                    className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-brown-500 peer"
                     placeholder=" "
-                    required
                   />
-                  <label htmlFor="state" className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">State</label>
+                  <label className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">State</label>
                 </div>
+
+            {/* Additional Comments Field */}
+<div className="relative z-0 w-full mb-5 group">
+<textarea
+  name="comments"
+  value={formData.comments}
+  onChange={handleInputChange}
+  className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-brown-500 peer"
+  placeholder=" "
+  rows="4" // Adjust rows to control textarea height
+  required
+/>
+<label className="peer-focus:font-medium absolute text-lg text-gray-900 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-brown-500 uppercase tracking-widest">
+  Additional Comments
+</label>
+</div>
 
                 <button
                   type="submit"
@@ -187,5 +193,5 @@ export default function Contact() {
         </div>
       </div>
     </>
-      );
+  );
 }
