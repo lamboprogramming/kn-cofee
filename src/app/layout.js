b7d508f29f9e901eadd76cc91bd1ba2b45fa19b7
Lamboprogramming <gillespielamar@gmail.com>
2024-11-24 20:02:18 -0500

    metadata changes to layoutjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,15 +15,22 @@ const geistMono = localFont({
 
 export const metadata = {
   title: "Kiwi Nutz Coffee",
-  description: "A New Zealand based Corporate coffee machine supplier, prioritizing quality, exceptional service, and great value.",
-  icons: {
-    icon: "./favicon.ico", 
-  },
+  description:
+    "A New Zealand based Corporate coffee machine supplier, prioritizing quality, exceptional service, and great value.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Metadata */}
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
