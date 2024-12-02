2347a4f9adabb502709f4a52083fe1adf3f0f0e7
Lamboprogramming <gillespielamar@gmail.com>
2024-12-02 10:46:32 -0500

    Meta tag added to head

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,12 +21,31 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-        <html lang="en">
-          <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-          >
-            {children}
-          </body>
-        </html>
+    <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {/* Google Tag Script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-X9N313DD2J"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X9N313DD2J');
+            `,
+          }}
+        ></script>
+      </Head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
