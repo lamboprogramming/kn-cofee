652cc76dea150225a0d37bca198d9ed444f7d53b
Lamar Gillespie <lamarg@mac.lan>
2024-12-24 14:37:32 -0500

    contact form style changes + footer

--- src/app/components/ContactInfo.jsx
+++ src/app/components/ContactInfo.jsx
@@ -5,18 +5,25 @@ import Link from 'next/link';
 
 export default function ContactInfo() {
   return (
-    <div className='p-4 mt-4'>
-      <div className="flex flex-col items-center justify-center mt-4 mb-6 space-y-4 sm:mt-8 sm:flex-row sm:space-x-8 sm:space-y-0">
-        <div className="flex items-center mb-4 space-x-2 font-serif text-lg font-semibold tracking-wide text-blue-900 sm:mb-0">
-          <FaPhoneAlt />
-          <span>(888) 542 - 5855</span>
-        </div>
-        <div className="flex items-center space-x-2 font-serif font-bold tracking-wider text-blue-900 font-lg">
-          <MdEmail />
-          <Link href="mailto:Support@kiwinutzcoffee.com">
-            <span>Support@kiwinutzcoffee.com</span>
+    <div className='pt-2 '>
+      <div className="flex sm:flex-col items-center justify-center mt-4 space-y-0.5 sm:mt-4 gap-4 ">
+
+        <div className="flex items-center space-x-0.5 font-serif font-thin tracking-wider text-blue-900 text-sm gap-0.5  ">
+          <div className='text-xs'><FaPhoneAlt /></div>
+          
+          <div className='ml-4'>(888)-542-5855</div>
+          </div>
+
+          <div className="flex items-center space-x-0.5 font-serif font-thin tracking-wider text-blue-900 text-sm gap-0.5">
+            <div className='text-xs'><MdEmail /></div>
+          
+          <Link href="mailto:Support@kiwinutzcoffee.com" className='cursor-pointer'>
+            <div>Support@kiwinutzcoffee.com</div>
           </Link>
         </div>
+
+        
+        
       </div>
     </div>
   );
